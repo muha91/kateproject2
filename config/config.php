183e89fb12c93411c4bc926cd1a564a76c8990d7
helen <kotik04@gmail.com>
2015-12-16 09:33:46 +0300

    config for kateproject

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_PORT", 3307);
+define("DB_PASS", "");
+define("DB_PORT", 3306);
 define("DB_NAME", "kateproject");
 
